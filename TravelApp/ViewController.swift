002e625a5d446950e710f8d3870cd26d101f7c43
Thanh Doi <thanh.doikhac@gmail.com>
2018-04-27 10:43:07 +0700

    first commit

---
+++ TravelApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelApp
+//
+//  Created by Thanh Doi on 4/27/18.
+//  Copyright © 2018 Thanh Doi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
